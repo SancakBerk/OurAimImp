0cf08d97b99c5f5a7c63ed18db0c97ccf8aefd2e
Berk Sancak <berksancak6161@gmail.com>
2025-11-10 14:27:56 +0300

    fix: deploy problem fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 import { isSessionExpired } from "@/utils/helperFunctions";
 import { redirect } from "next/navigation";
 import { useEffect } from "react";
-import { getCurrentExchangeRates } from "../services/globalService";
 
 const Home = () => {
   useEffect(() => {
